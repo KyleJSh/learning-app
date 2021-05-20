ecba6e713ea97bbda3e3d9ddd79b79e3508fe029
Kyle Sherrington <kylejsherrington@gmail.com>
2021-05-19 17:06:37 -0700

    Initial Commit

---
+++ Learning App/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Learning App
+//
+//  Created by Kyle Sherrington on 2021-05-19.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
