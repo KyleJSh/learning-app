364feca78ab43d197e9550b410752fbe4e117f1f
Kyle Sherrington <kylejsherrington@gmail.com>
2021-05-19 17:44:34 -0700

    Create model structs for json

---
+++ Learning App/Models/Models.swift
@@ -0,0 +1,51 @@
+//
+//  Models.swift
+//  Learning App
+//
+//  Created by Kyle Sherrington on 2021-05-19.
+//
+
+import Foundation
+
+struct Module: Identifiable, Decodable {
+    
+    var id: Int
+    var category: String
+    var content: Content
+    var test: Test
+}
+
+struct Content:Identifiable, Decodable {
+    
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var lessons: [Lesson]
+}
+
+struct Lesson:Identifiable, Decodable {
+    
+    var id: Int
+    var title: String
+    var video: String
+    var duration: String
+    var explanation: String
+}
+
+struct Test: Identifiable, Decodable {
+    
+    var id: Int
+    var image: String
+    var time: String
+    var description: String
+    var questions: [Question]
+}
+
+struct Question: Decodable, Identifiable {
+    
+    var id: Int
+    var content: String
+    var correctIndex: Int
+    var answers: [String]
+}
